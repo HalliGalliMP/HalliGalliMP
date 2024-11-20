6e0fb7ab6a96474165383be5808f412c4c337dda
이종우 <leejongwoo777@gmail.com>
2024-11-20 20:11:34 +0900

    아이콘, 카드 뒤 추가

--- HalliGalli/app/src/main/java/com/example/halligalli/Card.java
+++ HalliGalli/app/src/main/java/com/example/halligalli/Card.java
@@ -1,4 +1,7 @@
 package com.example.halligalli;
 
 public class Card {
+
+
+
 }
--- HalliGalli/app/src/main/java/com/example/halligalli/ResultActivity.java
+++ HalliGalli/app/src/main/java/com/example/halligalli/ResultActivity.java
@@ -9,5 +9,8 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+
     }
 }
